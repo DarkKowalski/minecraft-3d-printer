d1abe410d7ee581be98697c1a80ac00142a042eb
Kowalski Dark <darkkowalski2012@gmail.com>
2022-05-22 01:32:21 +0800

    Use clang as default compiler

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+CONFIG['CC'] = 'clang'
+CONFIG['CXX'] = 'clang++'
+
+$CFLAGS = ['-Wall -Werror=return-type -O3 -flto -march=native', $CFLAGS].join(' ')
+$CXXFLAGS = ['-Wall -Werror=return-type -O3 -flto -march=native', $CXXFLAGS].join(' ')
+
 require 'mkmf'
 create_makefile('minecraft_3d_printer')
